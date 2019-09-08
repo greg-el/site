78fe89aecb2d6d4e9d38478951a0368ba2ffefc6
Greg Leonard <gr3gl@hotmail.com>
2019-09-08 17:01:55 +0100

    udpates

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
+import './index.css'
 
 
 const summary = "Hi. My name is Greg and I'm a hobbiest programmer. I'm interested in things front/end/arduino related."
 
-function About() {
-  return (
-    <body>
-      <h1>About me</h1>
-      <p>{ summary }</p>
-    </body>
-  )
-}
+
+
+
+
+  
+
+const About = () => 
+  <div className="main">
+    <h1>About me</h1>
+    <p>{ summary }</p>
+    <Link />
+  </div>
+  
+const Link = () =>
+  <div className="links">
+    <a className="git" href="https://www.github.com/greg-el/">git</a>
+    <a className="linkedin" href="https://www.linkedin.com/in/gregory-leonard-ab3bbb191/">linkedin</a>
+  </div>
 
 
 ReactDOM.render(
-  main,
+  <About />,
+
   document.getElementById('root')
 )
 
